9a3d3adf0bd52ee80ed3ed97a0bbae3f7270fabd
Brady Brown <bbrown30@massmutual.com>
2019-11-04 19:32:50 -0500

    got custom env vars working

--- setupEnv.js
+++ setupEnv.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const {execSync} = require('child_process')
 
-const templateString = fs.readFileSync(".env-template.json")
-
-let template = JSON.parse(templateString)
+let template = {}
 
 /**
  * Add in your custom env vars here
  */
 template.REACT_APP_BRANCH_NAME = execSync("git rev-parse --symbolic-full-name --abbrev-ref HEAD").toString().trim()
+template.REACT_APP_VERSION = process.env.npm_package_version
+
 
 /**
  * Output result into 
